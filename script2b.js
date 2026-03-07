ee691ac0d00e76f7e1bda607fcf36e47d55b7bb4
penmawashi8-ux <penmawashi8@gmail.com>
2026-03-07 13:01:29 +0900

    script2b.js を更新

--- script2b.js
+++ script2b.js
@@ -63,14 +63,30 @@ var portfolio=window.cachedPortfolio;
 var raceId=typeof getRaceId==='function'?getRaceId():'';
 if(!portfolio||!portfolio.found){alert('ポートフォリオがありません');return;}
 if(!raceId||raceId.length!==12){alert('race_idが不正です');return;}
-var bets=portfolio.horses.map(function(h,i){return{horseNum:h.horse_num,amount:portfolio.bets[i]};});
-localStorage.setItem('umaca_bets',JSON.stringify({raceId:raceId,bets:bets,total:portfolio.total}));
-var msg='【購入内容】\n';
-for(var i=0;i<bets.length;i++){msg+=bets[i].horseNum+'番 単勝 '+bets[i].amount+'円\n';}
-msg+='合計: '+portfolio.total+'円\n\nウマカスマートを開きます。740画面でブックマークレット「自動入力」をタップしてください。';
-alert(msg);
-window.open('https://www.ipat.jra.go.jp/sp/umaca/','_blank');
+var venue=raceId.slice(4,6);
+var rHex=parseInt(raceId.slice(10,12)).toString(16).toUpperCase();
+var bits=[0x8000,0x4000,0x2000,0x1000,0x0800,0x0400,0x0200,0x0100,0x0080,0x0040,0x0020,0x0010,0x0008,0x0004,0x0002];
+var nbCodes=[];
+for(var i=0;i<portfolio.horses.length;i++){
+var h=portfolio.horses[i];
+var b=portfolio.bets[i];
+var bit=bits[h.horse_num-1]||0;
+var bitHex=bit.toString(16).toUpperCase().padStart(4,'0');
+var amtHex=Math.round(b/100).toString(16).toUpperCase().padStart(4,'0');
+nbCodes.push('"1'+'00'+venue+rHex+'701'+'8'+bitHex+'00000000000'+amtHex+'"');
 }
+var bl='javascript:(function(){';
+for(var i=0;i<nbCodes.length;i++){
+bl+='Nb['+i+']='+nbCodes[i]+';';
+}
+bl+='ToSend();})()';
+navigator.clipboard.writeText(bl).then(function(){
+alert('コピーしました！\nウマカスマート740画面のアドレスバーに貼り付けて実行してください');
+}).catch(function(){
+prompt('以下をコピーしてウマカスマート740のアドレスバーに貼り付けてください',bl);
+});
+}
+
 
 window.calculatePortfolio=calculatePortfolio;
 window.displayPortfolioInfo=displayPortfolioInfo;
