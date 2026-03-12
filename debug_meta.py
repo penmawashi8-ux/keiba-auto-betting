12190e1947fd160b0ca8f65f2116c87c67785a49
penmawashi8-ux <penmawashi8@gmail.com>
2026-03-13 08:59:07 +0900

    update debug_meta.py via Scriptable

--- debug_meta.py
+++ debug_meta.py
@@ -11,19 +11,19 @@ url = f'https://race.netkeiba.com/race/shutuba.html?race_id={race_id}'
 r = SESSION.get(url, timeout=15)
 html = r.content.decode('euc-jp', errors='replace')
 
-print(f'HTTP: {r.status_code}, len={len(html)}')
+print('=== è·é¢ã£ã½ãæ°å­ã®å¨è¾º ===')
+for m in re.finditer(r'\\d{3,4}m', html):
+    start = max(0, m.start()-80)
+    end = min(len(html), m.end()+40)
+    print(html[start:end].replace('\n',''))
 
-# 芝/ダート/距離っぽいキーワード周辺を全部表示
-for kw in ['芝', 'ダート', 'ダ', '1600', '距離', 'RaceData', 'race_type']:
-    for m in re.finditer(kw, html):
-        start = max(0, m.start()-30)
-        end   = min(len(html), m.end()+60)
-        print(f'[{kw}] ...{html[start:end]}...')
-        break  # 最初の1件だけ
-
-# クラス判定キーワード
-for kw in ['新馬', '未勝利', '1勝', 'オープン', 'GⅠ', 'G1']:
-    if kw in html:
-        idx = html.index(kw)
-        print(f'[class:{kw}] ...{html[max(0,idx-20):idx+50]}...')
+print('\n=== RaceDataç³»ã¯ã©ã¹å ===')
+for m in re.finditer(r'class="[^"]*[Rr]ace[^"]*"', html):
+    print(m.group())
+    if m.start() > 50000:
         break
+
+print('\n=== metaã¿ã° description ===')
+m = re.search(r'<meta[^>]+description[^>]+content="([^"]+)"', html)
+if m:
+    print(m.group(1))
