96d4abe8d0233f7635782c813dca3fa98cb67b23
penmawashi8-ux <penmawashi8@gmail.com>
2026-03-13 11:21:02 +0900

    update debug_meta.py via Scriptable

--- debug_meta.py
+++ debug_meta.py
@@ -1,17 +1,10 @@
-import requests, re
+import requests, re, csv
 
 SESSION = requests.Session()
 SESSION.headers.update({'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15', 'Referer': 'https://race.netkeiba.com/'})
 
-race_id = '202506010101'
-url = f'https://race.netkeiba.com/race/shutuba.html?race_id={race_id}'
-r = SESSION.get(url, timeout=15)
-html = r.content.decode('euc-jp', errors='replace')
-
-m = re.search(r'class="Race_Name"[^>]*>(.*?)</', html, re.DOTALL)
-if m:
-    print("Race_Name:", repr(m.group(1)[:200]))
-
-m2 = re.search(r'class="Race_Data"[^>]*>(.*?)</div>', html, re.DOTALL)
-if m2:
-    print("Race_Data:", repr(m2.group(1)[:300]))
+# enriched.csvã®æåã®5ä»¶ãç¢ºèª
+with open('enriched.csv', encoding='utf-8-sig') as f:
+    rows = list(csv.DictReader(f))
+for r in rows[:5]:
+    print(r['race_id'], r['surface'], r['distance'], r['dist_band'], r['race_class'])
